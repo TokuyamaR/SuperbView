c3ca56f4dc06a42034700926b0f7ae2f6fae965d
TokuyamaR <qpflapper.1007@gmail.com>
2018-08-07 12:27:54 +0000

    新規登録、ログイン、トップページのビューを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  	protected
+
+  	def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    		user_params.permit(:name, :email, :password, :password_confirmation, :current_password, :deleted_at, :accepted)
+        end
+  	end
+
+	def after_sign_in_path_for(resource)
+		case resource
+		when User
+    	root_path
+  	when Administrator
+   		root_path
+  	end
+	end
+
+	def after_sign_out_path_for(resource)
+    if resource == :user
+      root_path
+    else
+      root_path
+    end
+  end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+# ページごとに指定したSCSSのみを読み込ませる処理
+# spots
+Rails.application.config.assets.precompile += %w( spots/top.scss )
+
+# users
+Rails.application.config.assets.precompile += %w( users/registrations/new.scss )
+Rails.application.config.assets.precompile += %w( users/registrations/new.js )
+Rails.application.config.assets.precompile += %w( users/sessions/new.scss )
